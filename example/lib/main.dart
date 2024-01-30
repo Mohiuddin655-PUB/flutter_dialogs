518988cc6e71053398b55a01c59630db9d249907
Md. Mohi-Uddin <mohiuddin655.bd@gmail.com>
2024-01-30 19:31:26 +0600

    initial release

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app_dialogs/dialogs.dart';
+import 'package:flutter_androssy_dialogs/dialogs.dart';
 
 void main() {
   runApp(const MyApp());
